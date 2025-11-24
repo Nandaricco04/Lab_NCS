e7775fcdf253bf3e0cef8397c26b9fbef1ca1e32
Nandaricco04 <nanda.ricco777@gmail.com>
2025-11-24 20:33:04 +0700

    PBL

--- admin/login.php
+++ admin/login.php
@@ -48,7 +48,25 @@
 			</div>
 		</div>
 	</div>
-	
+
+	<div class="modal fade" id="errorModal" tabindex="-1" role="dialog" aria-labelledby="errorModalLabel" aria-hidden="true">
+	  <div class="modal-dialog" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h5 class="modal-title" id="errorModalLabel">Login Error</h5>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+	      <div class="modal-body" id="errorModalBody">
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-primary" data-dismiss="modal">Tutup</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 	<script src="vendor/animsition/js/animsition.min.js"></script>
 	<script src="vendor/bootstrap/js/popper.js"></script>
@@ -58,5 +76,13 @@
 	<script src="vendor/daterangepicker/daterangepicker.js"></script>
 	<script src="vendor/countdowntime/countdowntime.js"></script>
 	<script src="js/main.js"></script>
+	<script>
+		<?php if (isset($_GET['error'])): ?>
+			$(document).ready(function(){
+				$('#errorModalBody').text("<?php echo htmlspecialchars($_GET['error'], ENT_QUOTES); ?>");
+				$('#errorModal').modal('show');
+			});
+		<?php endif; ?>
+	</script>
 </body>
 </html>
